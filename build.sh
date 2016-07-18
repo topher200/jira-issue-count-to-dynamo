878e00a70ea61474118513541b636dffdb7cf5cf
Topher Brown <t.brown@wordstream.com>
2016-07-17 21:52:23 -0400

    add credentials file to package

--- build.sh
+++ build.sh
@@ -3,13 +3,16 @@
 ROOT_DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ZIP_FILENAME="package.zip"
 
-# remove old package file
+echo "removing old package file"
 rm $ROOT_DIRNAME/$ZIP_FILENAME
 
-# add virtualenv packages
+echo "adding virtualenv packages"
 cd ~/.virtualenvs/asserts/lib/python2.7/site-packages
 zip -r9 $ROOT_DIRNAME/$ZIP_FILENAME * &> /dev/null
 
-# add our script
+echo "adding our script"
 cd $ROOT_DIRNAME
-zip -g $ZIP_FILENAME run.py &> /dev/null
+zip -g $ZIP_FILENAME run.py
+
+echo "adding our credentials file"
+zip -g $ZIP_FILENAME .env
